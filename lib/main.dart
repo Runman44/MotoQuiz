5037922bc63fc7922dd464f50be6a3fe48db3087
Dennis Anderson <dennis@tellow.nl>
2020-07-27 16:17:43 +0200

    added 6 questions

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,15 @@ class _MyHomePageState extends State<MyHomePage> {
   final List<Question> _questions = [
     Question(
         question:
+        "Waarop controleer je de verlichting van je voertuig?",
+        answers: [
+          Answer(answer: "Op de helderheid van het glas"),
+          Answer(answer: "Op het functioneren en op de helderheid van het glas", isCorrect: true),
+          Answer(answer: "Op de helderheid van het glas en op de aanwezigheid van een reserveset lampen"),
+        ],
+        description: "Geen uitleg"),
+    Question(
+        question:
             "Welke volgafstand moet je tenminste aanhouden bij een snelheid van 100 kilometer per uur?",
         answers: [
           Answer(answer: "Ongeveer 35 meter"),
@@ -52,6 +61,14 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
         description: "De geleiders van rij- of trekdieren of van vee horen bij de categorie bestuurders. Net als ruiters. Iemand die met een paard aan de hand loopt is een geleider van een rijdier. Deze persoon is dus een bestuurder."),
     Question(
+        question: "Wat doe je voordat je met de motor een smalle tunnel inrijdt?",
+        answers: [
+          Answer(answer: "Je haalt niet in", isCorrect: true),
+          Answer(answer: "Je houdt dezelfde afstand als altijd"),
+          Answer(answer: "Je gaat links rijden")
+        ],
+        description: "Tunnel rijden vereist aangepast,alert rijgedrag ontsteek je dimlicht ruim voordat je de tunnel in rijd."),
+    Question(
         question: "Je gaat motorrijden. Welke documenten moet je bij je hebben?",
         answers: [
           Answer(answer: "Het rijbewijs"),
@@ -59,6 +76,14 @@ class _MyHomePageState extends State<MyHomePage> {
           Answer(answer: "Het rijbewijs en het kentekenbewijs en het verzekeringsbewijs")
         ],
         description: "Het rijbewijs en kentekenbewijs (kentekencard) moet je altijd tijdens een controle kunnen tonen. Het verzekeringsbewijs (groene kaart) is alleen nodig in het buitenland."),
+    Question(
+        question: "Wanneer kun je de bandenspanning het beste meten?",
+        answers: [
+          Answer(answer: "Direct na afloop van de rit"),
+          Answer(answer: "Voor vertrek", isCorrect: true),
+          Answer(answer: "Na een uurtje te hebben gereden")
+        ],
+        description: "Door te rijden neemt de temperatuur van de band toe en de spanning zal iets hoger zijn. Het is dus beter om de spanning voor vertrek te meten."),
     Question(
         question: "Wat gebeurt er met het brandstofverbruik als je goed anticipeert?",
         answers: [
@@ -84,6 +109,14 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
         description: "Volgens de wet wordt 'onder invloed rijden' gestraft met een gevangenisstraf van ten hoogste 1 jaar of een geldboete van 21.750 euro. Daarnaast kan er ook een bijkomende straf worden opgelegd, zoals het verplicht volgen van een cursus of het ontzeggen van de rijbevoegdheid."),
     Question(
+        question: "Hoeveel afstand moet je zijdelings minimaal nemen als je een fietser wilt inhalen?",
+        answers: [
+          Answer(answer: "1,5 M", isCorrect: true),
+          Answer(answer: "3 M"),
+          Answer(answer: "0,50 cm")
+        ],
+        description: "0,5 meter is dus te weinig. 3 meter is te veel, dan zit je op de verkeerde kant van de weg."),
+    Question(
         question: "Je bent 25 jaar en hebt alcohol gedronken. Mag je met een alcoholpercentage van 0,6 ‰ gaan rijden?",
         answers: [
           Answer(answer: "Zeker"),
@@ -107,12 +140,27 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
         description: "Het weggedrag voor voertuigen met winterbanden op een droog wegdek is anders dan met zomerbanden. Rijd daarom niet sneller dan wat de bandenfabrikant je adviseert. Dit kun je zien aan de snelheidscodering op de band."),
     Question(
+        question: "Met welk verschil in snelheid mag je een langzaam rijdende file inhalen?",
+        answers: [
+          Answer(answer: "maximaal 10 km/uur", isCorrect: true),
+          Answer(answer: "maximaal 30 km/uur"),
+          Answer(answer: "maximaal 50 km/uur")
+        ],
+        description: "Sommigen vinden het niet prettig als je inhaalt. Ze moeten zelf namelijk wachten en zijn daarom jaloers op jou. Het kan gebeuren dat ze het gat voor jou dichtgooien. Het kan ook zijn dat ze je niet zien en met allerlei dingen bezig zijn die niets met het verkeer te maken hebben."),
+    Question(
         question: "Je hebt een windscherm en kappen op je motor gezet. Waar let je op?",
         answers: [
           Answer(answer: "Je wordt meer tegengehouden door de wind. Daardoor kan je minder goed sturen"),
           Answer(answer: "Je wordt minder tegengehouden door de wind. Daardoor ga je harder rijden", isCorrect: true)
         ],
         description: "Door het gebruik van een windscherm en kappen op de motor zal de rijwind gestroomlijnder langs jou en de motor heen gaan. De winddruk op je lichaam en het windgeluid nemen hiermee af en daardoor het gevoel van snelheid. De neiging ontstaat om sneller te gaan rijden, houd daarom de snelheidsmeter goed in de gaten."),
+    Question(
+        question: "50 meter voor je rijdt een tractor en je nadert een kruispunt, waar moet je rekening mee houden?",
+        answers: [
+          Answer(answer: "Je moet bestuurders van tractoren tijdig naderen, want ze zien je aankomen ondanks de lading"),
+          Answer(answer: "Door de lading hebben bestuurders van tractoren vaak geen goed zicht naar achteren", isCorrect: true)
+        ],
+        description: "Houd er rekening mee dat de tractor een inrit kan opdraaien zonder dat je hem richting aan hebt zien geven. Vaak is de verlichting niet in orde of zit er modder op."),
     Question(
         question: "Bij een politiecontrole blijkt dat je voertuig te veel geluid maakt. Wat kan de politie doen?",
         answers: [
